40e67d76c39926801d14483cde14e97c238a12c1
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-05-23 00:06:46 +0400

    Fix ResultCard duplicate export

--- frontend/src/components/ResultCard.jsx
+++ frontend/src/components/ResultCard.jsx
@@ -1,15 +1,4 @@
-
 import React from "react";
-
-const ResultCard = ({ data }) => {
-  return (
-    <div className="card">
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
-};
-
-export default ResultCard;
 
 export default function ResultCard({ result }) {
   if (!result) return null;
