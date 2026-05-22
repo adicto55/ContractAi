5e208c93dd1f4000a1d7c70ef5813d73cef00599
niceehsn <hsnislam2026hellomyf@gmail.com>
2026-05-22 23:54:18 +0400

    Task

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,4 @@
-const BASE_URL =
-  process.env.REACT_APP_API_URL ||
-  "https://contractai-me2g.onrender.com";
 
+const BASE_URL = "https://contractai-me2g.onrender.com";
+const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
 export default BASE_URL;
